98254a51a541d16ec60a2957615d06be34024a7c
swlee <swo98@naver.com>
2021-12-17 20:34:40 +0900

    Add files via upload

---
+++ 01. 선택정렬/C/Selection_Sort.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable:6031)
+
+#include <stdio.h>
+
+int main()
+{
+	int i, j, min, idx, temp;
+	int arr[10] = { 1,5,6,10,2,9,4,8,7,3 };
+
+	for (i = 0; i < 10; i++) {
+		min = 999;
+		for (j = i; j < 10; j++) {
+			if (min > arr[j]) {
+				min = arr[j];
+				idx = j;
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = temp;
+	}
+	for (i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+}
+
